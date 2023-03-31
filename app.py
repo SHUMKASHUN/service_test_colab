eb8bbf1588dc4e918aef5243f50f78c99259f749
SHUMKASHUN <ksshumab@connect.ust.hk>
2023-03-31 16:02:53 +0800

    push

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from flask_cors import CORS
 import json
 # import torch
 # import os
-list_a = ["123","234","345"]
-type_list = ["1","2","1"]
+
 app = Flask(__name__)
 CORS(app)
 # ds_config_path = "../examples/ds_config.json"
@@ -28,33 +27,35 @@ CORS(app)
 # world_size = int(os.getenv("WORLD_SIZE", "1"))
 # torch.cuda.set_device(local_rank)
 # model = AutoModel.get_model(model_args, tune_strategy='none', ds_config=ds_config)
-@app.route('/predict',methods = ['POST','GET'])
+import time
+@app.route('/predict',methods = ['POST'])
 def predict():
-    print(request)
-    return "Hello2"
-
-@app.route('/',methods = ['POST','GET'])
-def login():
-    user_input = request.form.get('user_input')
-    print(request.form)
-    if user_input != None:
-        user_input = user_input.strip("\r").strip("\n").strip(" ")
-        print(len(user_input.replace(u'\xa0', u'').strip(" ")))
     if(request.method == "POST"):
-        text_out = user_input
-        list_a[2] = "cap"
+        try:
+            user_input = request.get_json()["Input"]
+            conversation = request.get_json()["History"]
+            print(conversation)
+            print(user_input)
+            text_out = "hey hey"
+        except:
+            text_out = "There is something wrong, please query again"
         # user_input is a string
         # inputs = model.encode(user_input, return_tensors="pt").to(device=local_rank)
-        # outputs = model.inference(inputs,min_length=5, max_length=100,temperature=0.0, do_sample=False)
+        # outputs = model.inference(inputs, max_new_tokens=100,temperature=0.0, do_sample=False)
         # text_out = model.decode(outputs[0], skip_special_tokens=True)
         # prompt_length = len(model.decode(inputs[0], skip_special_tokens=True,))
         # text_out = text_out[prompt_length:].strip("\n")
         # print(text_out)
     else:
         text_out = "pending"
-    print(list_a)
+    return text_out
 
-    return render_template('index.html',msg = text_out, demo_list = list_a, type_list = type_list, title_variable = "hhh")
+
+
+@app.route('/',methods = ['GET'])
+def login():
+
+    return render_template('index.html')
 
 
 app.run(port = 8888, debug = True)
